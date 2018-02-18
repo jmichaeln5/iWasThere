78142636085f5a42213fcd9445c63f6dd33343c0
Jon Norton <jmichaeln5@gmail.com>
2018-02-17 22:37:42 -0500

    added places, users, google maps api

---
+++ db/migrate/20180212025959_create_places.rb
@@ -0,0 +1,12 @@
+class CreatePlaces < ActiveRecord::Migration[5.1]
+  def change
+    create_table :places do |t|
+      t.string :title
+      t.text :description
+      t.integer :rating
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
